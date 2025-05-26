0562b86e335e9a762c9764b5a3e87d78d02e1e74
itz-cyber9235 <discordpurposes2@gmail.com>
2025-05-26 12:03:24 -0500

    texture overhaul (courtesy of NoxxNoxxy)

--- src/main/java/cybermods/cyberextras/block/ModBlocks.java
+++ src/main/java/cybermods/cyberextras/block/ModBlocks.java
@@ -14,7 +14,7 @@ import net.minecraft.util.Identifier;
 
 public class ModBlocks {
 
-public static final Block EYE_BLOCK = registerBlock("eye_block",
+public static final Block EYE_BLOCK = registerBlock("eye_block.png",
        new Block(AbstractBlock.Settings.copy(Blocks.IRON_BLOCK).sounds(BlockSoundGroup.SCULK)));
 
     private static Block registerBlock(String name, Block block){
