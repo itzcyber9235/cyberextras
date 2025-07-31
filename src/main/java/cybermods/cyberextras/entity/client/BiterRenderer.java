df5e6e387fed6191787622acfa33d7a3fcb588b2
itz-cyber9235 <discordpurposes2@gmail.com>
2025-07-30 23:09:11 -0500

    Attempting to add Biter entity

--- src/main/java/cybermods/cyberextras/entity/client/BiterRenderer.java
+++ src/main/java/cybermods/cyberextras/entity/client/BiterRenderer.java
@@ -6,16 +6,12 @@ import cybermods.cyberextras.entity.custom.BiterEntity;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.client.render.entity.MobEntityRenderer;
-import net.minecraft.client.render.entity.model.EntityModel;
 import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.entity.Entity;
 import net.minecraft.util.Identifier;
 
 public class BiterRenderer extends MobEntityRenderer<BiterEntity, BiterModel<BiterEntity>> {
-
-
     public BiterRenderer(EntityRendererFactory.Context context) {
-        super(context, new BiterModel<>(context.getPart(BiterModel.BITER)), 0.5f);
+        super(context, new BiterModel<>(context.getPart(BiterModel.BITER)), 0.75f);
     }
 
     @Override
@@ -26,13 +22,12 @@ public class BiterRenderer extends MobEntityRenderer<BiterEntity, BiterModel<Bit
     @Override
     public void render(BiterEntity livingEntity, float f, float g, MatrixStack matrixStack,
                        VertexConsumerProvider vertexConsumerProvider, int i) {
-        if (livingEntity.isBaby()) {
+        if(livingEntity.isBaby()) {
             matrixStack.scale(0.5f, 0.5f, 0.5f);
         } else {
             matrixStack.scale(1f, 1f, 1f);
-
-
-            super.render(livingEntity, f, g, matrixStack, vertexConsumerProvider, i);
         }
+
+        super.render(livingEntity, f, g, matrixStack, vertexConsumerProvider, i);
     }
 }
